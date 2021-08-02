e2884258f0fc9282cedaf3b98bbba895f57d23e7
Sid B <siddhesh.bgk@gmail.com>
2021-08-02 10:28:11 -0400

    email changed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -48,11 +48,11 @@ const Sidebar = () => {
                     <GoLocation />
                     <span>Mumbai, India</span>
                 </div>
-                <p className="my-2">sid.bgk@gmail.com</p>
+                <p className="my-2">siddhesh.bgk@gmail.com</p>
                 <p className="my-2">8149991128</p>
             </div>
             <button className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400 focus:outline-none"
-                onClick={() => window.open("mailto:sid.bgk@gmail.com")}
+                onClick={() => window.open("mailto:siddhesh.bgk@gmail.com")}
             >Email me</button>
             <button onClick={changeTheme} className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400">Toggle Theme</button>
         </div>
