3c58d750a8fcc2ae6feb004ead0296322af479a1
Siddhesh B <siddhesh.bhogaonkar@techno-soft.in>
2024-11-10 09:07:21 +0530

    updated chagnes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -35,11 +35,11 @@ const Sidebar = () => {
                 B
             </h3>
             <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200">Full Stack Developer</p>
-            <a className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200" href="/assets/siddhesh_bgk_2022.pdf" download="siddhesh_bgk_2022">
+            <a className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200" href="/assets/siddhesh_bgk_2024.pdf" download="siddhesh_bgk_2024.pdf">
                 <GiTie className="w-6 h-6" /> Download Resume
             </a>
             <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full">
-                <a href="https://www.linkedin.com/in/siddhesh-bhogaonkar-a15728172" target="_blank" rel="noreferrer" aria-label="LinkedIn">
+                <a href="https://www.linkedin.com/in/sid-bgk" target="_blank" rel="noreferrer" aria-label="LinkedIn">
                     <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
                 </a>
             </div>
@@ -48,11 +48,11 @@ const Sidebar = () => {
                     <GoLocation />
                     <span>Mumbai, India</span>
                 </div>
-                <p className="my-2">siddhesh.bgk@gmail.com</p>
-                <p className="my-2">8149991128</p>
+                <p className="my-2">siddhesh.bgk01@gmail.com</p>
+                <p className="my-2">+91 814 999 1128</p>
             </div>
             <button className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400 focus:outline-none"
-                onClick={() => window.open("mailto:siddhesh.bgk@gmail.com")}
+                onClick={() => window.open("mailto:siddhesh.bgk01@gmail.com")}
             >Email me</button>
             <button onClick={changeTheme} className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400">Toggle Theme</button>
         </div>
